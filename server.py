f0140adbd5662bead5b71a6a2f184b8f1a9068fa
emd5 <emahoney5@mail.greenriver.edu>
2018-08-22 09:03:51 -0700

    implemented the do_GET and do_POST method in server.py

--- server.py
+++ server.py
@@ -1,21 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 from cowpy import cow
-import httpie
 import os
+import json
 
-
-# cheese = cow.Beavis()
-
-# msg = cheese.milk('This is the message')
-
-# print(msg)
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # import pdb; pdb.set_trace()
+        """ gets the parsed url request"""
         parsed_path = urlparse(self.path)
-        print(parsed_path)
         parsed_qs = parse_qs(parsed_path.query)
 
         # set a status code
@@ -31,27 +24,59 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             return
 
         elif parsed_path.path == '/cow':
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.end_headers()
-            msg = parsed_qs['msg'][0]
-            print(cow.Ghostbusters().milk(httpie.msg))
-            self.wfile.write(cow.Ghostbusters().milk(msg).encode())
-            # self.wfile.write(b'<html><body><h1>Cow!</h1></body></html>')
-            return
 
-        self.send_response(404)
-        self.end_headers()
+            try:
+                cow_image = cow.Stegosaurus()
+                msg = cow_image.milk(parsed_qs['msg'][0])
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/html')
+                self.end_headers()
+                self.wfile.write(msg.encode())
+                return
+
+            except KeyError:
+                different_cow = cow.BongCow()
+                msg = different_cow.milk("400")
+                self.send_response(400)
+                self.send_header('Content-Type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(msg.encode())
+        else:
+            cow_ghost = cow.Ghostbusters()
+            msg = cow_ghost.milk("404")
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(msg.encode())
 
     def do_POST(self):
+        """ sends the parsed url request"""
         parsed_path = urlparse(self.path)
         parsed_qs = parse_qs(parsed_path.query)
 
-        if parsed_path.path == '/cow':
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json; charset=utf-8')
+        if parsed_path.path == '/':
+            try:
+                cow_image = cow.Beavis()
+                msg_post = cow_image.milk(parsed_qs['msg'][0])
+                json_image = json.dumps({'Content': msg_post})
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                print(json_image)
+                self.wfile.write(json_image.encode())
+                return
+            except KeyError:
+                self.send_response(400)
+                self.send_header('Content-Type', 'text/plain')
+                self.end_headers()
+                different_cow_image = cow.BongCow()
+                msg = different_cow_image.milk("400")
+                self.wfile.write(msg.encode())
+        else:
+            self.send_response(404)
             self.end_headers()
-            msg = parse_qs['msg'][0]
+            bad_cow = cow.DragonAndCow()
+            msg = bad_cow.milk("404")
+            self.wfile.write(msg.encode())
 
 
 def create_server():
@@ -59,6 +84,7 @@ def create_server():
         ('127.0.0.1', int(os.environ['PORT'])),
         SimpleHTTPRequestHandler
     )
+
 
 def run_forever():
     server = create_server()
